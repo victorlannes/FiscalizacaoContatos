3b1bb15893a5934c905bb3aeb282c8b99c4d7ad3
Victor Lannes <victor.ferreiralannes@hotmail.com>
2023-03-24 22:09:34 -0300

    Inserindo a funcionalidade dos botões de "Editar" e "Apagar" contatos.

--- FiscalizacaoContatos/Repositório/IContatoRepositorio.cs
+++ FiscalizacaoContatos/Repositório/IContatoRepositorio.cs
@@ -5,7 +5,12 @@ namespace FiscalizacaoContatos.Repositório
 {
     public interface IContatoRepositorio
     {
+        ContatoModel ListarPorId(int id);
         List<ContatoModel> BuscarTodos();
         ContatoModel Adicionar(ContatoModel contato);
+
+        ContatoModel Atualizar(ContatoModel contato);
+
+        bool Apagar(int id);
     }
 }
