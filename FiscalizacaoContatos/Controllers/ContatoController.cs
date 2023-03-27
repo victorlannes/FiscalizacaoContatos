46df7200fa6ef7ca9d281b8ef637b2455704b00e
Victor Lannes <victor.ferreiralannes@hotmail.com>
2023-03-27 17:49:16 -0300

    Validações dos formulários e mensagem de sucesso ou erro ao excluir um contato.

--- FiscalizacaoContatos/Controllers/ContatoController.cs
+++ FiscalizacaoContatos/Controllers/ContatoController.cs
@@ -34,23 +34,70 @@ namespace FiscalizacaoContatos.Controllers
 
         public IActionResult Apagar(int id)
         {
-            _contatoRepositorio.Apagar(id);
-            return RedirectToAction("Index");
+            try
+            {
+                bool apagado = _contatoRepositorio.Apagar(id);
+
+                if (apagado)
+                {
+                    TempData["MensagemSucesso"] = "Contato apagado com sucesso!";
+                }
+                else
+                {
+                    TempData["MensagemErro"] = "Ops, não conseguimos apagar seu contato!";
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (System.Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos apagar seu contato, mais detalhes do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
 
         }
 
         [HttpPost]
         public IActionResult Criar(ContatoModel contato)
         {
-            _contatoRepositorio.Adicionar(contato);
-            return RedirectToAction("Index");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _contatoRepositorio.Adicionar(contato);
+                    TempData["MensagemSucesso"] = "Contato cadastrado com sucesso";
+                    return RedirectToAction("Index");
+                }
+                return View(contato);
+
+            }
+            catch (System.Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos cadastrar seu contato, tente novamente. Detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+            
         }
 
         [HttpPost]
         public IActionResult Alterar(ContatoModel contato)
         {
-            _contatoRepositorio.Atualizar(contato);
-            return RedirectToAction("Index");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _contatoRepositorio.Atualizar(contato);
+                    TempData["MensagemSucesso"] = "Contato alterado com sucesso";
+                    return RedirectToAction("Index");
+                }
+                return View("Editar", contato);
+
+            }
+            catch (System.Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos atualizar seu contato, tente novamente. Detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
--- FiscalizacaoContatos/Models/ContatoModel.cs
+++ FiscalizacaoContatos/Models/ContatoModel.cs
@@ -1,10 +1,18 @@
-﻿namespace FiscalizacaoContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FiscalizacaoContatos.Models
 {
     public class ContatoModel
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do contato")]
         public string Nome { get; set;}
+        [Required(ErrorMessage = "Digite o e-mail do contato")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Digite o celular do contato")]
+        [Phone(ErrorMessage = "O celular informando não é válido")]
         public string Celular { get; set; } 
     }
 }
